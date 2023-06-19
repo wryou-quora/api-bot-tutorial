5c929dc638f897f0e76e0c1360ba70fe8390d6d1
Wonryong Ryou <wryou@quora.com>
2023-06-19 03:18:23 +0000

    echobot: add "you said" before echo

--- echobot.py
+++ echobot.py
@@ -17,7 +17,7 @@ from sse_starlette.sse import ServerSentEvent
 class EchoBot(PoeBot):
     async def get_response(self, query: QueryRequest) -> AsyncIterable[ServerSentEvent]:
         last_message = query.query[-1].content
-        yield self.text_event(last_message)
+        yield self.text_event("you said: " + last_message)
 
 
 if __name__ == "__main__":
